f385afa7fc71bac61011e0bedb40e7ce8eb5dd95
0318tetuo <0318tetuo@gmail.com>
2024-11-24 22:37:36 +0900

    a

--- baseball.js
+++ baseball.js
@@ -3,7 +3,7 @@ const play_screen = document.querySelector('#play_screen');
 
 game_start.addEventListener("click", game_start1);
 
-function game_start1{
+function game_start1(){
   game_start.style.display = 'none';
   play_screen.style.display = 'flex';
 }
