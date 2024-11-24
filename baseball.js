050d64709dbf1ed111e20667c4d87b9335b31123
0318tetuo <0318tetuo@gmail.com>
2024-11-24 22:30:54 +0900

    yatte-mi-yo

---
+++ baseball.js
@@ -0,0 +1,9 @@
+const game_start = document.querySelector('.game_start');
+const play_screen = document.querySelector('#play_screen');
+
+game_start.addEventListener("click", game_start1);
+
+function game_start1{
+  game_start.style.display = 'none';
+  play_screen.style.display = 'flex';
+}
