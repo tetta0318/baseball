50cedf0fc4e7c2af0e4ede5980a4885090fcea0a
0318tetuo <0318tetuo@gmail.com>
2024-11-25 22:20:46 +0900

    function

--- baseball.js
+++ baseball.js
@@ -5,7 +5,7 @@ const play_screen = document.querySelector('#play_screen');
   batter_ability = [弾道,ミート,パワー,走力,肩力,捕球,守備力]
   pitcher_ability = [球速,コントロール,スタミナ,変化球]
 */
-const A_ability = [ [2,48,50,96,63,61,41],
+const B_ability = [ [2,48,50,96,63,61,41],
                     [3,67,74,64,74,67,62],
                     [4,74,95,78,85,42,52],
                     [4,60,92,64,61,59,55],
@@ -15,9 +15,9 @@ const A_ability = [ [2,48,50,96,63,61,41],
                     [2,57,57,56,82,66,74],
                     [2,43,42,86,67,89,77]];
 
-const A_pitcher=[[152,71,68,72],[152,59,74,80],[159,54,51,80],[163,44,80,80]];
+const B_pitcher=[[152,71,68,72],[152,59,74,80],[159,54,51,80],[163,44,80,80]];
 
-const B_ability = [ [2,88,68,91,85,88,69],
+const A_ability = [ [2,88,68,91,85,88,69],
                     [3,73,72,71,68,50,65],
                     [3,85,68,78,64,75,69],
                     [3,50,72,54,88,75,38],
@@ -27,7 +27,7 @@ const B_ability = [ [2,88,68,91,85,88,69],
                     [3,71,60,70,73,70,77],
                     [3,55,49,84,53,75,67]];
 
-const B_pitcher = [157,80,84,90];
+const A_pitcher = [157,80,84,90];
 
 game_start.addEventListener("click", game_start1);
 
@@ -35,8 +35,7 @@ function game_start1(){
   game_start.style.display = 'none';
   play_screen.style.display = 'flex';
 
-    console.log(batting(A_ability[0][0], A_ability[0][1], A_ability[0][2], B_pitcher[0], B_pitcher[1], B_pitcher[2], B_pitcher[3]))
-  
+    //console.log(batting(A_ability[0][0], A_ability[0][1], A_ability[0][2], B_pitcher[0], B_pitcher[1], B_pitcher[2], B_pitcher[3]));
 }
 
 function getRandom (min, max){
@@ -56,4 +55,19 @@ function batting(tranjectory, meet, running_ability, ball_speed, control, breaki
     else A = 3;
   }
   return A;
+}
+
+function game_main(){
+  let times = 1, runner = 0, A_order = 0, B_order, B_P = 0;
+  let A = [0,0,0,0,0,0,0,0,0], B = [0,0,0,0,0,0,0,0,0];
+  let A_H = 0, B_H, A_R, B_R;
+
+
+}
+
+function time(){
+  let o = 0;
+  while(o < 3){
+    const batting_result = batting()
+  }
 }
